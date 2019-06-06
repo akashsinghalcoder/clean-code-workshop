d4a033bc72c5e66c6ee93b53056c62271d9c1318
Akash Singhal <akash@technogise.com>
2019-06-06 14:21:36 +0530

    extracted out amount calculation logic from customer class to rental class

--- src/main/java/com/movierental/Customer.java
+++ src/main/java/com/movierental/Customer.java
@@ -19,33 +19,14 @@ public class Customer {
     return name;
   }
   
-  private double calculateAmount(Rental rental) {
-	  double thisAmount = 0;
-      //determine amounts for each line
-      switch (rental.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (rental.getDaysRented() > 2)
-            thisAmount += (rental.getDaysRented() - 2) * 1.5;
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += rental.getDaysRented() * 3;
-          break;
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (rental.getDaysRented() > 3)
-            thisAmount += (rental.getDaysRented() - 3) * 1.5;
-          break;
-      }
-      return thisAmount;
-  }
+  
 
   public String statement() {
     double totalAmount = 0;
     int frequentRenterPoints = 0;
     String result = "Rental Record for " + getName() + "\n";
     for (Rental each : rentals) {
-    	double thisAmount = calculateAmount(each);
+    	double thisAmount = each.calculateAmount();
       
       // add frequent renter points
       frequentRenterPoints++;
